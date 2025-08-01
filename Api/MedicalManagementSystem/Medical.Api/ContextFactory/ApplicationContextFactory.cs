f875a9cce18d74dbe6023f261e5f10846d4d683b
Freddy Ordonez Aguilar <freddy12aguilar@outlook.com>
2025-07-31 19:20:39 -0600

    Create FactoryContext EFC

---
+++ Api/MedicalManagementSystem/Medical.Api/ContextFactory/ApplicationContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Medical.Persistence.ContextFactory
+{
+    public class ApplicationContextFactory : IDesignTimeDbContextFactory<ApplicationContext>
+    {
+        public ApplicationContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseSqlServer(config.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly("Medical.Api"));
+
+            return new ApplicationContext(builder.Options);
+        }
+    }
+}
