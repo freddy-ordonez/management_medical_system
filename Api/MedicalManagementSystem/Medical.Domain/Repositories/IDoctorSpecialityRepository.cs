f8724c224a739145142a7e13e2fbb47842425572
Freddy Ordonez Aguilar <freddy12aguilar@outlook.com>
2025-08-03 21:39:15 -0600

    Create funtions doctor speciality repository

--- Api/MedicalManagementSystem/Medical.Domain/Repositories/IDoctorSpecialityRepository.cs
+++ Api/MedicalManagementSystem/Medical.Domain/Repositories/IDoctorSpecialityRepository.cs
@@ -1,7 +1,11 @@
-﻿namespace Medical.Domain.Repositories
+﻿using Medical.Domain.Entities;
+
+namespace Medical.Domain.Repositories
 {
     public interface IDoctorSpecialityRepository
     {
-
+        Task<IEnumerable<DoctorSpecialty>> FindByIdDoctorSpeciality(int doctorId, bool trackChanges);
+        void CreateDoctorSpeciality(DoctorSpecialty doctorSpecialty);
+        void DeleteDoctorSpeciality(DoctorSpecialty doctorSpecialty);
     }
 }
---
+++ Api/MedicalManagementSystem/Medical.Persistence/Repositories/DoctorSpecialityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Medical.Domain.Entities;
+using Medical.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Medical.Persistence.Repositories
+{
+    internal class DoctorSpecialityRepository : RepositoryBase<DoctorSpecialty>, IDoctorSpecialityRepository
+    {
+        public DoctorSpecialityRepository(ApplicationContext applicationContext) : base(applicationContext)
+        {
+        }
+
+        public void CreateDoctorSpeciality(DoctorSpecialty doctorSpecialty) => Create(doctorSpecialty);
+
+        public void DeleteDoctorSpeciality(DoctorSpecialty doctorSpecialty) => Delete(doctorSpecialty);
+
+        public async Task<IEnumerable<DoctorSpecialty>> FindByIdDoctorSpeciality(int doctorId, bool trackChanges) =>
+            await FindByCondition(ds => ds.DoctorID.Equals(doctorId), trackChanges)
+                    .Include(ds => ds.Specialty)
+                    .ToListAsync();
+    }
+}
