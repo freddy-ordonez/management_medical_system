d4d9225632fcd72de4ee78a5c74ec4b8127287e2
Freddy Ordonez Aguilar <freddy12aguilar@outlook.com>
2025-08-21 18:30:17 -0600

    Create funtions users repository

--- Api/MedicalManagementSystem/Medical.Domain/Repositories/IUserRepository.cs
+++ Api/MedicalManagementSystem/Medical.Domain/Repositories/IUserRepository.cs
@@ -1,7 +1,12 @@
-﻿namespace Medical.Domain.Repositories
+﻿using Medical.Domain.Entities;
+
+namespace Medical.Domain.Repositories
 {
     public interface IUserRepository
     {
-
+        Task<IEnumerable<User>> FindAllUsers(bool trackChanges);
+        Task<User> FindUserById(int id, bool trackChanges);
+        void CreateUser(User user);
+        void DeleteUser(User user);
     }
 }
---
+++ Api/MedicalManagementSystem/Medical.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using Medical.Domain.Entities;
+using Medical.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Medical.Persistence.Repositories
+{
+    public class UserRepository : RepositoryBase<User>, IUserRepository
+    {
+        public UserRepository(ApplicationContext applicationContext) : base(applicationContext)
+        {
+        }
+
+        public void CreateUser(User user) => Create(user);
+
+        public void DeleteUser(User user) => Delete(user);
+
+        public async Task<IEnumerable<User>> FindAllUsers(bool trackChanges) =>
+            await FindAll(trackChanges)
+                    .ToListAsync();
+
+        public async Task<User?> FindUserById(int id, bool trackChanges) =>
+            await FindByCondition(u => u.UserID.Equals(id), trackChanges)
+                    .Include(u => u.Roles)
+                    .SingleOrDefaultAsync();
+    }
+}
