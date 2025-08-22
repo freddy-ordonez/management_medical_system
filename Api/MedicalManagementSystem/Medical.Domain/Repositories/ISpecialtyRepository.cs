a13efb479765e5ae72d9a4e8b892dec9105e781d
Freddy Ordonez Aguilar <freddy12aguilar@outlook.com>
2025-08-21 18:14:32 -0600

    Create funtions specialties repository

--- Api/MedicalManagementSystem/Medical.Domain/Repositories/ISpecialtyRepository.cs
+++ Api/MedicalManagementSystem/Medical.Domain/Repositories/ISpecialtyRepository.cs
@@ -1,7 +1,11 @@
-﻿namespace Medical.Domain.Repositories
+﻿using Medical.Domain.Entities;
+
+namespace Medical.Domain.Repositories
 {
     public interface ISpecialtyRepository
     {
-
+        Task<IEnumerable<Specialty>> FindAllSpecialty(bool trackChanges);
+        void CreateSpecialty(Specialty specialty);
+        void DeleteSpecialty(Specialty specialty);
     }
 }
---
+++ Api/MedicalManagementSystem/Medical.Persistence/Repositories/SpecialtyRepository.cs
@@ -0,0 +1,21 @@
+﻿using Medical.Domain.Entities;
+using Medical.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Medical.Persistence.Repositories
+{
+    public class SpecialtyRepository : RepositoryBase<Specialty>, ISpecialtyRepository
+    {
+        public SpecialtyRepository(ApplicationContext applicationContext) : base(applicationContext)
+        {
+        }
+
+        public void CreateSpecialty(Specialty specialty) => Create(specialty);
+
+        public void DeleteSpecialty(Specialty specialty) => Delete(specialty);
+
+        public async Task<IEnumerable<Specialty>> FindAllSpecialty(bool trackChanges) =>
+            await FindAll(trackChanges)
+                    .ToListAsync();
+    }
+}
