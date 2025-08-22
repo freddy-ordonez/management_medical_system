17f244fcabbce34390e1c21c6199ae37b1486431
Freddy Ordonez Aguilar <freddy12aguilar@outlook.com>
2025-08-21 18:08:43 -0600

    Create funtions roles repository

--- Api/MedicalManagementSystem/Medical.Domain/Repositories/IRoleRepository.cs
+++ Api/MedicalManagementSystem/Medical.Domain/Repositories/IRoleRepository.cs
@@ -1,7 +1,11 @@
-﻿namespace Medical.Domain.Repositories
+﻿using Medical.Domain.Entities;
+
+namespace Medical.Domain.Repositories
 {
     public interface IRoleRepository
     {
-
+        Task<IEnumerable<Role>> FindAllRoles(bool trackChanges);
+        void CreateRole(Role role);
+        void DeleteRole(Role role);
     }
 }
---
+++ Api/MedicalManagementSystem/Medical.Persistence/Repositories/RoleRepository.cs
@@ -0,0 +1,21 @@
+﻿using Medical.Domain.Entities;
+using Medical.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Medical.Persistence.Repositories
+{
+    public class RoleRepository : RepositoryBase<Role>, IRoleRepository
+    {
+        public RoleRepository(ApplicationContext applicationContext) : base(applicationContext)
+        {
+        }
+
+        public void CreateRole(Role role) => Create(role);
+
+        public void DeleteRole(Role role) => Delete(role);
+
+        public async Task<IEnumerable<Role>> FindAllRoles(bool trackChanges) =>
+            await FindAll(trackChanges)
+                    .ToListAsync();
+    }
+}
